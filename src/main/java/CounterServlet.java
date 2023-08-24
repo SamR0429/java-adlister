41b7d829b7c31985962117c377703725d9829bbf
SamR0429 <Samanatha.ramos01@gmail.com>
2023-08-24 15:20:03 -0500

    finished servlet exercise

---
+++ src/main/java/CounterServlet.java
@@ -0,0 +1,21 @@
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/count")
+
+
+public class CounterServlet extends HttpServlet {
+
+    public static int views = 0;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException{
+        views++;
+
+        res.getWriter().println("<h1>Hello counter</h1>" + views );
+    }
+
+}
