adb5ee3f85cdd51c21863797ab5db3b70fd0e755
SamR0429 <Samanatha.ramos01@gmail.com>
2023-09-01 11:22:50 -0500

    added code

--- src/main/java/JDBCEmployees.java
+++ src/main/java/JDBCEmployees.java
@@ -1,21 +1,23 @@
 import java.sql.*;
+import com.mysql.cj.jdbc.Driver;
+
 
 public class JDBCEmployees {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
 
         DriverManager.registerDriver(new Driver());
 
         Connection connection = DriverManager.getConnection(
                 "jdbc:mysql://localhost:3306/codeup_test_db?allowPublicKeyRetrieval=true&useSSL=false",
-                "root",
-                "codeup"
+                "root2",
+                "tryagain"
         );
 
         Statement statement = connection.createStatement();
 
         ResultSet rs = statement.executeQuery("SELECT first_name FROM employees LIMIT 10");
         while (rs.next()){
-            System.out.println(rs.getString());
+            System.out.println(rs.getString(1));
         }
 
 
